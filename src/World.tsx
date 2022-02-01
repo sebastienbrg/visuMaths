23e5835e7c48ac93e54d5da2031f9677f249135e
Sebastien Berge <sebastien.berge@vossloh.com>
2022-02-01 13:59:18 +0100

    * working on tens ...

--- src/World.tsx
+++ src/World.tsx
@@ -1,11 +1,6 @@
-import { useEffect, useRef, useState } from "react";
-import Matter, { Body } from "matter-js"
-import MatterAttractors from "matter-attractors"
-Matter.use(MatterAttractors);
+import {useEffect, useRef, useState} from "react";
+import Matter from "matter-js"
 
-
-const CanWidth = 1200;
-const CanHeight = 800;
 // module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
@@ -13,30 +8,30 @@ var Engine = Matter.Engine,
     Bodies = Matter.Bodies,
     Composite = Matter.Composite;
 
+const UnitWidth = 30;
+const UnitHeight = 20;
 
-const createAttractor = (allBodies, index) => {
-    const the_attr = Matter.Bodies.rectangle(CanWidth / 3 * index, 50, 40, 40, {
+
+const unit = (x: number, y: number, w: number, h: number, world: any) => {
+    let bod = null;
+    console.log("Created an attracted")
+    bod = Bodies.rectangle(x, y, w, h, {
         density: 0.4,
-        friction: 0.001,
-        frictionAir: 0.00001,
-        restitution: 0.8,
-        plugin: {
-            attractors: [
-                (bodyA: any, bodyB: any) => {
-                    return {
-                        x: (bodyA.position.x - bodyB.position.x) * 1e-3,
-                        y: (bodyA.position.y - bodyB.position.y) * 1e-3,
-                      };
-                }
-            ]
-        }
-    })
-    allBodies.push(the_attr)
+        friction: 0.01,
+        frictionAir: 0.0001,
+        restitution: 0.7,
+    });
+    Matter.World.add(world, bod);
+    return bod;
 }
 
 const World = () => {
     const canvasRef = useRef<any>(null);
-    const [firstNumber, setFirstNumber] = useState<number>(400);
+    const [firstNumber, setFirstNumber] = useState<number>(22);
+    const [secondNumber, setSecondNumber] = useState<number>(22);
+    const engine_ref = useRef<any>(null);
+    const myBodies_ref = useRef<any[]>([]);
+    const myBodies2_ref = useRef<any[]>([]);
     useEffect(() => {
         if (canvasRef.current) {
             const CanWidth = canvasRef.current.clientWidth;
@@ -60,87 +55,20 @@ const World = () => {
             engine.world.gravity.scale = 0.0001;
 
             // create two boxes and a ground
-            const ground = Bodies.rectangle(CanWidth / 2, CanHeight - 20, CanWidth + 100, 60, { isStatic: true });
+            const ground = Bodies.rectangle(CanWidth / 2, CanHeight - 20, CanWidth + 100, 60, {isStatic: true});
             const wallLeft = Bodies.rectangle(5, CanHeight / 2, 10, CanHeight, {
-                 isStatic: true,
-                 render: {
-                     visible:false
-                 }
-                });
-            const wallRight = Bodies.rectangle(CanWidth-5, CanHeight / 2, 10, CanHeight, {
-                    isStatic: true,
-                    render: {
-                        visible:false
-                    }
-                   });
-            const allBodies = [ground, wallLeft, wallRight];
-
-            const cercle = (x: number, y: number, rayon: number) => {
-                allBodies.push(Bodies.circle(x, y, rayon, {
-                    density: 0.4,
-                    friction: 0.01,
-                    frictionAir: 0.00001,
-                    restitution: 1,
-                    render: {
-                        fillStyle: '#F35e66',
-                        strokeStyle: 'black',
-                        lineWidth: 1
-                    }
-                }));
-            }
-            let logged = 0;
-            const carre = (x: number, y: number, largeur: number, attractIntensity: number) => {
-                let bod = null;
-                if (attractIntensity > 0) {
-                    createAttractor(allBodies, attractIntensity);
-                    console.log("Created an attractor")
-                    return;
-
-                    bod = Bodies.rectangle(CanWidth / 3, 50, 40, 40, {
-                        isStatic: true,
-                        density: 1,
-                        plugin: {
-                            attractors: [
-                                (bodyA: any, bodyB: any) => {
-                                    return {
-                                        x: (bodyA.position.x - bodyB.position.x) * 1e-4,
-                                        y: (bodyA.position.y - bodyB.position.y) * 1e-4,
-                                    };
-                                }
-                            ]
-                        }
-                    });
-                    // plugin["attractors"] = [];
-                    // plugin["attractors"].push(
-                    //     (bodyA: any, bodyB: any) => {
-                    //         return {
-                    //             x: (bodyA.position.x - bodyB.position.x) * 1e-4,
-                    //             y: (bodyA.position.y - bodyB.position.y) * 1e-4,
-                    //         };
-                    //     }
-                    // );
-                } else {
-                    console.log("Created an attracted")
-                    bod = Bodies.rectangle(x, y, largeur, largeur, {
-                        density: 0.4,
-                        friction: 0.01,
-                        frictionAir: 0.0001,
-                        restitution: 0.8,
-                    });
+                isStatic: true,
+                render: {
+                    visible: false
                 }
-                allBodies.push(bod)
-            }
-
-
-
-            // Exemples
-            //cercle(10, 10, 10);
-            carre(CanWidth / 2 - 50, 40, 40, 1);
-            //cercle(1160, 10, 10);
-            // Ecris la dessous
-            carre(CanWidth / 2 + 50, 120, 60, 2);
-
-            //createAttractor(allBodies, 3)
+            });
+            const wallRight = Bodies.rectangle(CanWidth - 5, CanHeight / 2, 10, CanHeight, {
+                isStatic: true,
+                render: {
+                    visible: false
+                }
+            });
+            Composite.add(engine.world, [ground, wallLeft, wallRight]);
 
             var mouseConstraint = Matter.MouseConstraint.create(engine, { //Create Constraint
                 element: canvasRef.current,
@@ -153,9 +81,7 @@ const World = () => {
             } as any);
             Matter.World.add(engine.world, mouseConstraint);
 
-            // add all of the bodies to the world
-            Composite.add(engine.world, allBodies);
-
+            engine_ref.current = engine;
             // run the renderer
             Render.run(render);
 
@@ -167,11 +93,110 @@ const World = () => {
         }
 
         return () => {
-
+            Matter.World.clear(engine.world, false);
+            Matter.Engine.clear(engine);
+            render.canvas.remove();
+            render.canvas = null;
+            render.context = null;
+            render.textures = {};
         }
     }, []);
-    //style={{ width: "" + CanWidth + "px", height: "" + CanHeight + "px", overflow: "auto" }} 
-    return <div style={{ height: "100%" }} ref={canvasRef}> </div>
+
+    useEffect(() => {
+        if (engine_ref.current) {
+
+            for (let i = 0; i < firstNumber; ++i) {
+                myBodies_ref.current.push(unit(220 - Math.random() * 200, 40 + Math.random() * 40, UnitWidth, UnitHeight, engine_ref.current.world));
+            }
+        }
+        return () => {
+            if (myBodies_ref.current) {
+                myBodies_ref.current.forEach(b => Matter.World.remove(engine_ref.current.world, b))
+                myBodies_ref.current = [];
+            }
+        }
+    }, [myBodies_ref, engine_ref, firstNumber])
+
+    useEffect(() => {
+        if (engine_ref.current) {
+            for (let i = 0; i < secondNumber; ++i) {
+                myBodies2_ref.current.push(unit(canvasRef.current.clientWidth - 220 + Math.random() * 200, 40 + Math.random() * 40, UnitWidth, UnitHeight, engine_ref.current.world));
+            }
+        }
+        return () => {
+            if (myBodies2_ref.current) {
+                myBodies2_ref.current.forEach(b => Matter.World.remove(engine_ref.current.world, b))
+                myBodies2_ref.current = [];
+            }
+        }
+    }, [myBodies_ref, engine_ref, secondNumber])
+
+    function attachTwoUnits(unit1, unit2, word) {
+        const options = {
+            bodyA: unit1,
+            bodyB: unit2,
+            pointA: {x: -20, y: 20},
+            pointB: {x: -20, y: 20},
+            style: "spring",
+            length: 2,
+            stiffness: 0.6
+        }
+        const constraint1 = Matter.Constraint.create(options);
+        const options2 = {...options};
+        options2.pointA = {x: -20, y: -20};
+        options2.pointB = {x: 20, y: 20};
+        const constraint2 = Matter.Constraint.create(options2);
+        Matter.World.add(engine_ref.current.world, [constraint1, constraint2]);
+    }
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            const groupCount = Math.floor(firstNumber / 10);
+            for (let groupIndex = 0; groupIndex < groupCount; ++groupIndex) {
+                for (let itemIndex = 1; itemIndex < 10; ++itemIndex) {
+                    attachTwoUnits(myBodies_ref.current[groupIndex * 10 + itemIndex - 1],
+                        myBodies_ref.current[groupIndex * 10 + itemIndex],
+                        engine_ref.current.world)
+                }
+            }
+            // console.log(engine_ref.current.world.bodies, myBodies_ref.current);
+            // Matter.Body.setStatic(myBodies_ref.current[0], true);
+            // const options = {
+            //     bodyA: myBodies_ref.current[0],
+            //     bodyB: myBodies_ref.current[1],
+            //     pointA: {x: -20, y: 20},
+            //     pointB: {x: -20, y: 20},
+            //     style: "spring",
+            //     length: 2,
+            //     stiffness: 0.6
+            // }
+            // const constraint1 = Matter.Constraint.create(options);
+            // const options2 = {...options};
+            // options2.pointA = {x: -20, y: -20};
+            // options2.pointB = {x: 20, y: 20};
+            // const constraint2 = Matter.Constraint.create(options2);
+            // Matter.World.add(engine_ref.current.world, [constraint1, constraint2]);
+        }, 4000)
+    })
+    ;
+//style={{ width: "" + CanWidth + "px", height: "" + CanHeight + "px", overflow: "auto" }}
+    return <div style={{height: "100%"}}>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+            <input type={"number"} value={firstNumber} onChange={e => {
+                const val = parseInt(e.target.value)
+                setFirstNumber(val);
+            }
+            }/>
+            <div>+</div>
+            <input type={"number"} value={secondNumber} onChange={e => {
+                const val = parseInt(e.target.value)
+                setSecondNumber(val);
+            }
+            }/>
+        </div>
+        <div style={{height: "100%"}} ref={canvasRef}></div>
+    </div>
 }
 
 export default World;
